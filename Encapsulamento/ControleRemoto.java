deae04f4b38fb2921baee2234d9677c41327aaaa
NiccolasFurigo <niccolas.furigo.souza@gmail.com>
2021-04-06 13:44:15 -0300

    Método Construtor

--- Encapsulamento/ControleRemoto.java
+++ Encapsulamento/ControleRemoto.java
@@ -1,5 +1,74 @@
 package Encapsulamento;
 
 public class ControleRemoto implements Controlador {
+
+    private int volume;
+    private boolean ligado;
+    private boolean tocando;
+
+    public ControleRemoto() {
+        this.volume = 50;
+        this.ligado = true;
+        this.tocando = false;
+    }
+    @Override
+    public void ligar() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void desligar() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void abrirMenu() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void fecharMenu() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void maisVolume() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void menosVolume() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void ligarMudo() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void desligarMudo() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void play() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void pause() {
+        // TODO Auto-generated method stub
+        
+    }
     
 }

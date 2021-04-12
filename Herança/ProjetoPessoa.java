6d6ca7cd49ea730a34f34bc794b798cb26fd6f18
NiccolasFurigo <niccolas.furigo.souza@gmail.com>
2021-04-12 19:34:47 -0300

    ProjetoPessoa Complete

--- Herança/ProjetoPessoa.java
+++ Herança/ProjetoPessoa.java
@@ -1,5 +1,21 @@
 package Herança;
 
 public class ProjetoPessoa {
-    
+
+    public static void main(String[] args){
+        Pessoa p1 = new Pessoa();
+        Aluno p2 = new Aluno();
+        Professor p3 = new Professor();
+        Funcionario p4 = new Funcionario();
+        
+        p1.setNome("Paulo");
+        p2.setNome("Maria");
+        p3.setNome("José");
+        p4.setNome("Fabiana");
+        
+        p2.setCurso("Informatica");
+        p3.setSalario(2500.6f);
+        p4.setSetor("Estoque");
+        
+    }
 }

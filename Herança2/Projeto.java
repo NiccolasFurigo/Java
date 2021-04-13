802dac0ea8013837193de62c7cb8275e3b96590a
NiccolasFurigo <niccolas.furigo.souza@gmail.com>
2021-04-13 16:32:11 -0300

    Herança1 Complete

--- Herança2/Projeto.java
+++ Herança2/Projeto.java
@@ -2,6 +2,23 @@ package Herança2;
 
 public class Projeto {
     public static void main(String[] args){
-        
+        Visitante v1 = new Visitante();
+        v1.setNome("Jorge");
+        v1.setSexo("H");
+        v1.setIdade(35);
+
+        Bolsista b1 = new Bolsista();
+        b1.setNome("Maria");
+        b1.setIdade(20);
+        b1.setSexo("M");
+
+        Aluno n1 = new Aluno();
+        n1.setNome("André");
+        n1.setIdade(40);
+        n1.setSexo("H");
+
+        System.out.println(v1.toString());
+        System.out.println(b1.toString());
+        System.out.println(n1.toString());
     }
 }
--- Herança2/Visitante.java
+++ Herança2/Visitante.java
@@ -4,6 +4,9 @@ public class Visitante extends Pessoa {
     private String estrangeiro;
 
     public String getEstrangeiro(){
-        
+        return estrangeiro;
+    }
+    public void setEstrangeiro(String estrangeiro){
+        this.estrangeiro = estrangeiro;        
     }
 }

9c45bd96d112f159c529acc003449b77110f8f7a
NiccolasFurigo <niccolas.furigo.souza@gmail.com>
2021-04-13 16:25:56 -0300

    Class Bosista Complete

--- Herança2/Bosista.java
+++ Herança2/Bosista.java
@@ -1,5 +1,21 @@
 package Herança2;
 
 public class Bosista extends Pessoa {
+    private float bolsa;
+
+    public void renovarBolsa(){
+        System.out.println("Renovando bolsa do " + this.nome);
+    }
     
+    public void pagarMensalidade(){
+        System.out.println(this.nome + " é bolsista! Pagando mensalidade facilitada");
+    }
+
+    public float getBolsa() {
+        return bolsa;
+    }
+
+    public void setBolsa(float bolsa) {
+        this.bolsa = bolsa;
+    }
 }
--- Herança2/Visitante.java
+++ Herança2/Visitante.java
@@ -1,5 +1,9 @@
 package Herança2;
 
 public class Visitante extends Pessoa {
-    
+    private String estrangeiro;
+
+    public String getEstrangeiro(){
+        
+    }
 }

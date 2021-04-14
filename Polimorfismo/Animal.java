b4ae8dab535b2bc2d7d177fbfc33ba1bff9084bc
NiccolasFurigo <niccolas.furigo.souza@gmail.com>
2021-04-14 15:46:44 -0300

    Class Animal Complete

---
+++ Polimorfismo/Animal.java
@@ -0,0 +1,10 @@
+package Polimorfismo;
+
+public abstract class Animal {
+    protected float peso;
+    protected int idade;
+    protected int membros;
+
+    public abstract void emitirSom();
+    
+}
---
+++ Polimorfismo/Programa.java
@@ -0,0 +1,7 @@
+package Polimorfismo;
+
+public class Programa {
+    public static void main(String[] args){
+        
+    }
+}

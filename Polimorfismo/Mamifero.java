ea3f4afaba7292f4e141ce327375adc3077b4de8
NiccolasFurigo <niccolas.furigo.souza@gmail.com>
2021-04-14 15:46:58 -0300

    Class Mamifero Complete

--- Polimorfismo/Mamifero.java
+++ Polimorfismo/Mamifero.java
@@ -1,6 +1,11 @@
 package Polimorfismo;
 
 public class Mamifero extends Animal {
+    protected String corPelo;
     
+    @Override
+    public void emitirSom() {
+        System.out.println("som de Mamífero");
+    }
     
 }

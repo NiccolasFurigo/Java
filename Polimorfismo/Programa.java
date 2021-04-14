a34c5cfba5ab4a20cdf03acadce5296ffafc74a2
NiccolasFurigo <niccolas.furigo.souza@gmail.com>
2021-04-14 15:58:50 -0300

    Programa Complete

--- Polimorfismo/Programa.java
+++ Polimorfismo/Programa.java
@@ -2,6 +2,11 @@ package Polimorfismo;
 
 public class Programa {
     public static void main(String[] args){
-        
+        Cachorro c = new Cachorro();
+
+        c.reagir("Olá");
+        c.reagir(11, 45);
+        c.reagir(true);
+        c.reagir(2, 17.5f);
     }
 }
